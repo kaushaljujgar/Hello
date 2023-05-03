0a30c90dcd948db0c4d3bd4ee78147ab8601205f
Kaushal <kaushaljujgar1@gmail.com>
2023-05-03 14:21:34 +0530

    Initial Commit.

---
+++ Hello/src/com/jspiders/Hello/App.java
@@ -0,0 +1,5 @@
+package com.jspiders.Hello;
+
+public class App {
+
+}
---
+++ Hello/src/com/jspiders/Hello/Hello.java
@@ -0,0 +1,8 @@
+package com.jspiders.Hello;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
